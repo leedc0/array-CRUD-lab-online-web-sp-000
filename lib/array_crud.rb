7f782d3b2b1c386b14ea970879c2ffc0eb36e9f4
Daniel Lee <leedc@me.com>
2019-07-20 23:52:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << []
-  puts add_element_to_end_of_array.inspect
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
